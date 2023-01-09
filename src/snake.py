05255f5c33cd8031a3b5f3c6d7d852d643b552b7
Tomas Zeman <tomaas.zeman@gmail.com>
2023-01-09 22:49:48 +0100

    Head sprite rotating

--- src/snake.py
+++ src/snake.py
@@ -7,15 +7,24 @@ from src.screen import Screen
 class Snake:
     def __init__(self, screen: Screen):
         self.screen = screen
-        # pg.image.load()
         self.head = pg.rect.Rect(0, 0, Constant.TILE_SIZE, Constant.TILE_SIZE)
         self.head.center = get_random_position()
         self.length = 1
         self.segments = [self.head.copy()]
         self.direction = (0, 0)
 
+        self.sprites = {
+            sprite: pg.transform.scale(pg.image.load(f"sprites/{sprite}.png"), (Constant.TILE_SIZE, Constant.TILE_SIZE))
+            for sprite in ["head", "tail", "body", "curve"]
+        }
+        self.rendered_sprites = {
+            sprite: self.sprites[sprite].copy()
+            for sprite in ["head", "tail", "body", "curve"]
+        }
+
     def draw(self):
-        [pg.draw.rect(self.screen.surface, "white", segment) for segment in self.segments]
+        # [pg.draw.rect(self.screen.surface, "white", segment) for segment in self.segments]
+        self.screen.surface.blit(self.rendered_sprites["head"], self.head)
 
     def move(self):
         self.head.move_ip(self.direction)
@@ -41,11 +50,15 @@ class Snake:
             prev_direction = self.direction
             if event.key == pg.K_UP:
                 self.direction = (0, -Constant.TILE_SIZE)
+                self.rendered_sprites["head"] = pg.transform.rotate(self.sprites["head"], -90)
             if event.key == pg.K_DOWN:
                 self.direction = (0, Constant.TILE_SIZE)
+                self.rendered_sprites["head"] = pg.transform.rotate(self.sprites["head"], 90)
             if event.key == pg.K_LEFT:
                 self.direction = (-Constant.TILE_SIZE, 0)
+                self.rendered_sprites["head"] = self.sprites["head"]
             if event.key == pg.K_RIGHT:
                 self.direction = (Constant.TILE_SIZE, 0)
+                self.rendered_sprites["head"] = pg.transform.rotate(self.sprites["head"], 180)
             if all([abs(current) - abs(previous) == 0 for current, previous in zip(self.direction, prev_direction)]):
                 self.direction = prev_direction
